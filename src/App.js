c4ccf16f7d02a36bc1bb1eacd2d5ed9f42765606
zeecnla <csrmelchor@gmail.com>
2020-07-21 10:17:07 -0700

    Finished the radnom quote page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
 import React, { useState, useEffect } from "react"
+import { Link, BrowserRouter, Route } from "react-router-dom"
 import "./App.css"
 
-const App = () => {
+const Quotes = (props) => {
+  const { author } = props
+  const [authorQuotes, setAuthorQuotes] = useState([])
+  useEffect(() => {
+    const page = Math.floor(Math.random() * 5) + 1
+    fetch(
+      `https://quote-garden.herokuapp.com/api/v2/authors/${author}?page=${page}&limit=3`
+    )
+      .then((resp) => {
+        return resp.json()
+      })
+      .then(({ quotes }) => {
+        console.log(quotes)
+        setAuthorQuotes(quotes)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+  console.log(" bruhrhuruhrhurhurhuur")
+  return (
+    <div>
+      <h1>{author}</h1>
+      <div>
+        <ul>
+          {authorQuotes.forEach(({ quoteText }) => (
+            <li>
+              <h3>{quoteText}</h3>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+const App = () => (
+  <BrowserRouter>
+    <div className="sans-serif">
+      <Route path="/" component={Main} />
+      <Route path="/author" component={Quotes} />
+    </div>
+  </BrowserRouter>
+)
+
+const Main = () => {
   const [quote, setQuote] = useState()
   const [author, setAuthor] = useState()
 
@@ -25,7 +71,6 @@ const App = () => {
   const getQuote = () => {
     fetch("https://quote-garden.herokuapp.com/api/v2/quotes/random")
       .then((resp) => {
-        console.log(resp)
         return resp.json()
       })
       .then(({ quote }) => {
@@ -39,10 +84,6 @@ const App = () => {
       })
   }
 
-  const getAuthorQuotes = () = {
-
-  }
-
   return (
     <div className="App">
       <header className="App-header">
@@ -54,13 +95,13 @@ const App = () => {
           random
         </button>
       </header>
+
       <main>
         <h1>{quote}</h1>
-
-        <button onClick={()=>{}}>
+        <Link to="/author" author={author}>
           <h3>{author}</h3>
           <span class="material-icons">arrow_right_alt</span>
-        </button>
+        </Link>
       </main>
       <footer>Cesar Melchor @ DevChallenges.io</footer>
     </div>
